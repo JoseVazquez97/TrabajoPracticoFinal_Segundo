55e15811f0cc59a2778ae614bd00ee4043cc28f6
Jose Ignacio Vazquez <vazquezjoseignacio22@gmail.com>
2022-11-07 11:39:18 -0300

    Se esta aprendiendo a usar SingalR

--- WebApi_SingalR_Com/Program.cs
+++ WebApi_SingalR_Com/Program.cs
@@ -1,7 +1,9 @@
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
@@ -10,6 +12,7 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    
     app.UseHsts();
 }
 
@@ -21,5 +24,6 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
+
 
 app.Run();
