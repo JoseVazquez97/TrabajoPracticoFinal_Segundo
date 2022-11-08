aef88c300840b071662463ca7f8773f4a7e609d3
Jose Vazquez <vazquezjoseingacio22@gmail.com>
2022-11-08 16:56:47 -0300

    Ahora no me anda ni el menssageboX

---
+++ TrabajoPracticoFinalSegundo/UserControls/Comunicador.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrabajoPracticoFinalSegundo.UserControls
+{
+    public partial class Comunicador : UserControl
+    {
+        public Comunicador()
+        {
+            InitializeComponent();
+            
+        }
+
+        public ref Label miLabel() 
+        {
+            return ref this.LABEL_PRUEBA;
+        }
+
+        public void siguienteValor(int i)
+        { 
+            this.LABEL_PRUEBA.Text = i.ToString();
+        }
+
+    }
+}
