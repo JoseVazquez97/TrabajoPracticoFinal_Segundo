5e4db3a68dc5271aa9a42dcd41e70870816ace1e
LittleCat27 <gregoirejoaquin@gmail.com>
2022-12-20 21:02:43 -0300

    Otros pequeños detalles visuales

--- TrabajoPracticoFinalSegundo/UserControls/Urna.Designer.cs
+++ TrabajoPracticoFinalSegundo/UserControls/Urna.Designer.cs
@@ -34,8 +34,13 @@
             // 
             // btn_Si
             // 
+            this.btn_Si.BackColor = System.Drawing.Color.Transparent;
             this.btn_Si.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Si.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_Si.FlatAppearance.BorderSize = 0;
+            this.btn_Si.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.btn_Si.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btn_Si.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.btn_Si.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_Si.Font = new System.Drawing.Font("BlackPearl", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btn_Si.Location = new System.Drawing.Point(19, 22);
@@ -44,13 +49,18 @@
             this.btn_Si.Size = new System.Drawing.Size(218, 82);
             this.btn_Si.TabIndex = 0;
             this.btn_Si.Text = "SI CAPITAN!";
-            this.btn_Si.UseVisualStyleBackColor = true;
+            this.btn_Si.UseVisualStyleBackColor = false;
             this.btn_Si.Click += new System.EventHandler(this.btn_Si_Click);
             // 
             // btn_No
             // 
+            this.btn_No.BackColor = System.Drawing.Color.Transparent;
             this.btn_No.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_No.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_No.FlatAppearance.BorderSize = 0;
+            this.btn_No.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.btn_No.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btn_No.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
             this.btn_No.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_No.Font = new System.Drawing.Font("BlackPearl", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btn_No.Location = new System.Drawing.Point(255, 22);
@@ -58,7 +68,7 @@
             this.btn_No.Size = new System.Drawing.Size(218, 82);
             this.btn_No.TabIndex = 1;
             this.btn_No.Text = "NO CAPITAN!";
-            this.btn_No.UseVisualStyleBackColor = true;
+            this.btn_No.UseVisualStyleBackColor = false;
             this.btn_No.Click += new System.EventHandler(this.btn_No_Click);
             // 
             // Urna
